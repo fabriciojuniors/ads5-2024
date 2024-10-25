bd3c135ecb1122b79c027040300dca77c551bf9b
fabricio.junior <fabriciojuniorsc@gmail.com>
2024-10-25 20:17:27 -0300

    Utilizando ícones e botões

--- primerio-projeto/src/app/home/home.page.ts
+++ primerio-projeto/src/app/home/home.page.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonInput } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonInput, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { starOutline}  from 'ionicons/icons'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonInput, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonInput, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({starOutline});
+  }
+
+public minhaFuncao() {
+    alert("Clicou no botão")
+}
+
 }
